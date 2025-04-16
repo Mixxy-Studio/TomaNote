d66a94752c1251d9b77ad7f2d476ee81e9f31f6c
camiicode <dftp93@gmail.com>
2025-04-15 20:57:59 -0500

    [0704202515] Update buttons on tab
    This buttons are for edit tab title and delete this tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro';
+import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      assetsDir: 'assets',
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name].[hash][extname]',
+          chunkFileNames: 'assets/[name].[hash].js',
+          entryFileNames: 'assets/[name].[hash].js',
+        },
+      },
+    }
   },
   site: 'https://camiicode.github.io',
   base: '/notepad',
   build: {
-    assets: '_assets'
+    assets: 'assets'
   }
 });
